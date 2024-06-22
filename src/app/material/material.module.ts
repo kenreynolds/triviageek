7d9c63e3b74f9a1a150d83f62c4f27799a0e282d
Ken Reynolds <kenreynolds78@gmail.com>
2024-06-22 00:39:03 -0500

    Refactor checkboxes to radio buttons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from "@angular/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatRadioModule } from "@angular/material/radio";
 import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   declarations: [],
-  imports: [MatCheckboxModule, MatFormFieldModule, MatSelectModule],
-  exports: [MatCheckboxModule, MatFormFieldModule, MatSelectModule],
+  imports: [
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+  ],
+  exports: [
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+  ],
 })
 export class MaterialModule {}
