4718417e279315da9a50d569936f2de97b70b9ef
Ken Reynolds <kenreynolds78@gmail.com>
2024-12-02 09:29:18 -0600

    Add missing imports so unit tests will pass

--- src/app/components/trivia-form/trivia-form.component.spec.ts
+++ src/app/components/trivia-form/trivia-form.component.spec.ts
@@ -3,21 +3,25 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { MaterialModule } from 'src/app/material/material.module';
+import { AppService } from 'src/app/service/app.service';
 import { TriviaFormComponent } from './trivia-form.component';
 
 describe('TriviaFormComponent', () => {
   let component: TriviaFormComponent;
   let fixture: ComponentFixture<TriviaFormComponent>;
+  let appService: AppService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ FormsModule, HttpClientTestingModule, MaterialModule, ReactiveFormsModule ],
-      declarations: [ TriviaFormComponent ]
+      declarations: [ TriviaFormComponent ],
+      providers: [ AppService ],
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TriviaFormComponent);
     component = fixture.componentInstance;
+    appService = TestBed.inject(AppService);
     fixture.detectChanges();
   });
 
